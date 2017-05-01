1c9c96cecbfd91683849a5c02cb7313a1066f3cf
Chprsky <chipirosky@outlook.es>
2017-05-01 18:25:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var Photo = {
   farm : undefined;
 }
 
-
+/*
 function getAllPhotos(data) {
   url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" +
     api_key + "&user_id=" + user_id + "&format=json&nojsoncallback=1";
@@ -37,12 +37,38 @@ function inyectHtml (url, size, description) {
 }
 
              
-function getPhotosBySearch (word) {
 
-  
-  }
              
 //Funcion para limpiar los formularios
 function cleanSearch (word){
   $(word).val("");
 }
+
+*/
+
+$(function() {
+$.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+	 + api_key + '&user_id=' +user_id + 
+	'&format=json&nojsoncallback=1',
+	mostrar_fotos
+)
+
+function mostrar_fotos(info){
+	var i;
+	for (i=0;i<info.photos.photo.length;i++) {
+	   var item = info.photos.photo[i];
+	   var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
+		          +'/'+item.id+'_'+item.secret+'_m.jpg';
+	   console.debug(url);
+	   $("#imagenes").append($("<img/>").attr("src",url));
+    }
+}
+
+function searchByText (text) {
+  $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+	 + api_key + '&text' + text + 
+	'&format=json&nojsoncallback=1',
+	mostrar_fotos);
+  
+}
+)
